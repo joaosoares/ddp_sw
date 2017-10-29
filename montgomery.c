35b014bd922011391c503d38391fbd990a8dbd22
João Soares <joaofelipe1395@gmail.com>
2017-10-29 13:49:03 +0100

    removed invocation of multiply_and_sum with possible overflow

--- montgomery.c
+++ montgomery.c
@@ -29,8 +29,9 @@ void mont(uint32_t *a, uint32_t *b, uint32_t *n, uint32_t *n0, uint32_t *res,
     S = multiply_and_sum(m, n[0], S, &C);
     int j;
     for (j = 1; j < size; j++) {
-      S = multiply_and_sum(a[j], b[i], t[j] + C, &C);
-      add_carry(t, j + 1, C);
+      sum = (uint64_t)t[j] + (uint64_t)a[j] * (uint64_t)b[i] + (uint64_t)C;
+      S = (uint32_t)sum;
+      C = (uint32_t)(sum >> 32);      add_carry(t, j + 1, C);
       S = multiply_and_sum(m, n[j], S, &C);
       t[j - 1] = S;
     }
